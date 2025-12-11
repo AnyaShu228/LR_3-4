3469db04a35fd66c077bf97b66132ffb3aaa70fd
AnyaShu228 <anna.shubin.97@mail.ru>
2025-12-11 17:38:33 +0300

    Add files via upload

---
+++ 3.5 (3).pas
@@ -0,0 +1,20 @@
+﻿program students;
+var 
+  N,c,i: integer;
+  o: array [1..3] of integer;
+  
+ begin
+   writeln ('введите количество студентов');
+   readln(N);
+   c:=0;
+   i:=0;
+   repeat
+   begin
+     writeln('введите оценки студента ',i+1);
+     readln(o[1],o[2],o[3]);
+     if (2 not in o) and (3 not in o) then c:=c+1;
+     i:=i+1;
+   end;
+   until i=N;
+   writeln('количество студентов без 3 и 2: ',c);
+ end.
