94986b844b41bd418da384e0089b851ec1d5a609
AnyaShu228 <anna.shubin.97@mail.ru>
2025-12-11 16:03:30 +0300

    Add files via upload

---
+++ 3.5.pas
@@ -0,0 +1,17 @@
+﻿program students;
+var 
+  N,c,i: integer;
+  o: array [1..3] of integer;
+  
+ begin
+   writeln ('введите количество студентов');
+   readln(N);
+   c:=0;
+   for i:=1 to N do
+   begin
+     writeln('введите оценки студента ',i);
+     readln(o[1],o[2],o[3]);
+     if (2 not in o) and (3 not in o) then c:=c+1;
+   end;
+   writeln('количество студентов без 3 и 2: ',c);
+ end.
---
+++ 3.5.py
@@ -0,0 +1,9 @@
+N=int(input())
+sum=0
+c=1
+while c<=N:
+    o=[]
+    a,b,c=map(int,input().split())
+    if 2 not in o and 3 not in o:
+        sum+=1
+    c+=1
